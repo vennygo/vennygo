5d60050b90d6f6e88c7d2d5dc6755a54b8a1c6eb
Bard-K <tngksmf103@gmail.com>
2024-05-05 00:10:18 +0900

    Initial commit

---
+++ src/main/java/com/vennygo/VennygoApplication.java
@@ -0,0 +1,13 @@
+package com.vennygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VennygoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VennygoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vennygo/VennygoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vennygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VennygoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
